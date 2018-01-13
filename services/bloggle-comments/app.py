d7a14d9be0f07037f8c4d03e047a056a2c6fd906
Memory Clutter <memclutter@gmail.com>
2018-01-13 08:00:23 +0300

    Use migrations

--- services/bloggle-comments/app.py
+++ services/bloggle-comments/app.py
@@ -1,5 +1,8 @@
 import os
 
-from common import create_app
+from flask_migrate import Migrate
+
+from common import create_app, db
 
 app = create_app(os.getenv('APP_ENV'))
+migrate = Migrate(app, db)
