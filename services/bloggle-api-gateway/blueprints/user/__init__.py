5ea0be7c393a9b2fac6f3fe6af252919563b9019
Memory Clutter <memclutter@gmail.com>
2018-01-21 23:17:49 +0300

    User sign up

--- services/bloggle-api-gateway/blueprints/user/__init__.py
+++ services/bloggle-api-gateway/blueprints/user/__init__.py
@@ -1,3 +1,4 @@
+import re
 from flask import Blueprint, request, jsonify, current_app
 from flask_jwt_extended import create_access_token, jwt_required, get_jwt_identity
 
@@ -11,6 +12,54 @@ user_blueprint = Blueprint('users', __name__, url_prefix='/users')
 def me():
     current_user = get_jwt_identity()
     return jsonify(dict(success=True, data=current_user)), 200
+
+
+@user_blueprint.route('/sign-up', methods=['POST'])
+def sign_up():
+    data = request.get_json()
+    if not data:
+        return jsonify(dict(success=False, message='Invalid payload')), 400
+
+    email = data.get('email')
+    password = data.get('password')
+    first_name = data.get('first_name')
+    last_name = data.get('last_name')
+
+    users_endpoint = current_app.config['ENDPOINTS']['users']
+    users = ServiceClient(users_endpoint)
+
+    errors = dict()
+    if not email:
+        errors['email'] = 'Email address is required'
+    elif not re.match(r'[^@]+@[^@]+\.[^@]+', email):
+        errors['email'] = 'Invalid email address'
+    else:
+        try:
+            ok, data, message = users.get('/users', dict(email=email))
+        except RuntimeError as e:
+            return jsonify(dict(success=False, message=str(e))), 500
+
+        if len(data) > 0:
+            errors['email'] = 'This email already use'
+
+    if not password:
+        errors['password'] = 'Password is required'
+
+    if len(errors) > 0:
+        return jsonify(dict(success=False, data=errors, message='Validation failed')), 422
+
+    try:
+        ok, data, message = users.post('/users', dict(email=email,
+                                                      password=password,
+                                                      first_name=first_name,
+                                                      last_name=last_name))
+    except RuntimeError as e:
+        return jsonify(dict(success=False, message=str(e))), 500
+
+    if not ok:
+        return jsonify(dict(success=False, message=message))
+
+    return jsonify(dict(success=True, data=data)), 201
 
 
 @user_blueprint.route('/auth', methods=['POST'])
