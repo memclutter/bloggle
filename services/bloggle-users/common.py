3b0c49b7f2c894455150257a668114065cef8dba
Memory Clutter <memclutter@gmail.com>
2018-01-23 20:14:34 +0300

    Create helpers.py

--- services/bloggle-users/common.py
+++ services/bloggle-users/common.py
@@ -1,42 +1,10 @@
-from flask import Flask, Response
-from flask import json
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 from config import environments
+from helpers import format_response
 
 db = SQLAlchemy()
-
-
-def format_response(response: Response):
-    """Format JSON response"""
-    status_code = response.status_code
-    success = (status_code >= 200) and (status_code <= 299)
-    content_type = response.headers.get('content-type', 'plain/text')
-    is_json = content_type == 'application/json'
-    body = json.loads(response.get_data()) if is_json else response.get_data().decode('utf-8')
-    message = None
-    data = None
-
-    if isinstance(body, str):
-        message = body
-    elif isinstance(body, dict):
-        if 'message' in body:
-            message = body.pop('message')
-
-        if 'data' in body:
-            data = body.pop('data')
-        else:
-            data = body
-    else:
-        data = body
-
-    response.headers['content-type'] = 'application/json'
-    response.set_data(json.dumps(dict(success=success,
-                                      status_code=status_code,
-                                      message=message,
-                                      data=data)))
-
-    return response
 
 
 def create_app(environment):
