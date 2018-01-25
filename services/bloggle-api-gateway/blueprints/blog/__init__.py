278e7ea1f92062fabb052a8f9a010e4234e276fb
Memory Clutter <memclutter@gmail.com>
2018-01-25 12:05:59 +0300

    Blogs my and all

--- services/bloggle-api-gateway/blueprints/blog/__init__.py
+++ services/bloggle-api-gateway/blueprints/blog/__init__.py
@@ -14,58 +14,48 @@ def create():
         return jsonify(dict(success=False, message='Invalid payload'))
 
     current_user = get_jwt_identity()
-    user_guid = current_user.get('guid')
-
-    title = data.get('title')
-    about = data.get('about')
-
-    errors = dict()
-
-    if not title:
-        errors['title'] = 'Title is required'
-
-    if len(errors) > 0:
-        return jsonify(dict(success=False, data=errors, message='Validation failed')), 422
+    data['user_guid'] = current_user.get('guid')
 
     blogs_endpoint = current_app.config['ENDPOINTS']['blogs']
     blogs = ServiceClient(blogs_endpoint)
 
-    try:
-        ok, data, message = blogs.post('/blogs', dict(user_guid=user_guid, title=title, about=about))
-    except RuntimeError as e:
-        return jsonify(dict(success=False, message=str(e))), 500
+    success, status_code, data, message = blogs.post('/blogs', data)
 
-    if not ok:
-        return jsonify(dict(success=False, message=message)), 400
-
-    return jsonify(dict(success=True, data=data)), 201
+    if success:
+        return jsonify(data), status_code
+    else:
+        return jsonify(message=message, data=data), status_code
 
 
 @blog_blueprint.route('', methods=['GET'])
 @jwt_required
 def index():
-    current_user = get_jwt_identity()
-    user_guid = current_user.get('guid')
+    data = dict(request.args)
+
     blogs_endpoint = current_app.config['ENDPOINTS']['blogs']
     blogs = ServiceClient(blogs_endpoint)
 
-    try:
-        page = int(request.args.get('page', 1))
-        page_size = int(request.args.get('page_size', 10))
+    success, status_code, data, message = blogs.get('/blogs', data)
 
-        if (page <= 0) or (page_size <= 0):
-            raise ValueError
-    except ValueError:
-        return jsonify(dict(success=False, message='Invalid payload')), 400
+    if success:
+        return jsonify(data), status_code
+    else:
+        return jsonify(message=message, data=data), status_code
 
-    try:
-        ok, data, message = blogs.get('/blogs', dict(page=page,
-                                                     page_size=page_size,
-                                                     user_guid=user_guid))
-    except RuntimeError as e:
-        return jsonify(dict(success=False, message=str(e))), 500
 
-    if not ok:
-        return jsonify(dict(success=False, message=message)), 400
+@blog_blueprint.route('/my', methods=['GET'])
+@jwt_required
+def index_my():
+    data = dict(request.args)
 
-    return jsonify(dict(success=True, data=data)), 200
+    current_user = get_jwt_identity()
+    data['user_guid'] = current_user.get('guid')
+    blogs_endpoint = current_app.config['ENDPOINTS']['blogs']
+    blogs = ServiceClient(blogs_endpoint)
+
+    success, status_code, data, message = blogs.get('/blogs', data)
+
+    if success:
+        return jsonify(data), status_code
+    else:
+        return jsonify(message=message, data=data), status_code
